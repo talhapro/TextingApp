2a3d225ecd82b0e63a6129191f3bd5aa86cbc52c
Talha <talha@soloking.pro>
2020-04-26 03:19:50 +0500

    End of section 8

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(scr => scr.Photos.FirstOrDefault(p => p.isMain).url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(scr => scr.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(scr => scr.Photos.FirstOrDefault(p => p.isMain).url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(scr => scr.DateOfBirth.CalculateAge()));   
+            CreateMap<Photo, PhotosForDetailedDto>();
+            
+        }
+
+    }
+}
